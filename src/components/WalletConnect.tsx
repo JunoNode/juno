0b9ecdbe6f54f2d23a1184c19483bea881f11dc1
Juno <junoaiagents@gmail.com>
2025-05-05 16:56:04 +1000

    Create WalletConnect.tsx

---
+++ src/components/WalletConnect.tsx
@@ -0,0 +1,10 @@
+export default function WalletConnect() {
+  return (
+    <div className="mt-4">
+      <button className="bg-jungle-glow text-black font-semibold px-4 py-2 rounded-xl hover:opacity-90 transition">
+        Connect Wallet
+      </button>
+      <p className="text-xs opacity-50 mt-1">Phantom & MetaMask supported</p>
+    </div>
+  );
+}
