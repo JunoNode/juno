69f493de3a118dc30fe7c2ae596453587acd3274
Juno <junoaiagents@gmail.com>
2025-05-26 20:06:43 +1000

    expand Tailwind config with full content paths and custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./index.html",
+    "./index.tsx",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./types/**/*.{ts}"
   ],
   theme: {
     extend: {
