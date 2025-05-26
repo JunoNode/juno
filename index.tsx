e78917a4799d0c5a30446922128bce0f4ced5621
Juno <junoaiagents@gmail.com>
2025-05-26 20:03:31 +1000

    hook up WalletDashboard to root with dummy data

--- index.tsx
+++ index.tsx
@@ -1,5 +1,18 @@
-// /src/pages/index.tsx
-export default function Home() {
-  return <div>Juno is waking up.</div>;
-}
- 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WalletDashboard from './components/WalletDashboard';
+
+const dummyWallet = {
+  address: "9gzLP8xR9dRka...vU7W",
+  totalSignals: 24,
+  topSignalType: "trend-reversal",
+  avgConfidence: 0.82,
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <WalletDashboard {...dummyWallet} />
+  </React.StrictMode>
+);
+
