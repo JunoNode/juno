6c4ae8fba611587204ee0dde55e2e5c73fdf2998
Juno <junoaiagents@gmail.com>
2025-05-26 19:52:57 +1000

    chore: define base weights per signal type

---
+++ constants/scoring.ts
@@ -0,0 +1,7 @@
+export const SIGNAL_TYPE_WEIGHTS: Record<string, number> = {
+  'trend-reversal': 0.6,
+  'volume-spike': 0.5,
+  'momentum': 0.4,
+  'sell-pressure': 0.3,
+  'buy-pressure': 0.3,
+};
