8b03b1b52a104e718e3f741824444148bba4d90b
Juno <junoaiagents@gmail.com>
2025-05-27 22:36:43 +1000

    integrate WalletTokenTable with mock token data below dashboard

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import WalletDashboard from './components/WalletDashboard';
+import WalletTokenTable from './components/WalletTokenTable';
 import './index.css';
-
 
 const dummyWallet = {
   address: "9gzLP8xR9dRka...vU7W",
@@ -11,10 +11,18 @@ const dummyWallet = {
   avgConfidence: 0.82,
 };
 
+const mockTokens = [
+  { name: 'Solana', symbol: 'SOL', amount: 12.3, usdValue: 242.15 },
+  { name: 'Bonk', symbol: 'BONK', amount: 98219323, usdValue: 118.56 },
+  { name: 'USDC', symbol: 'USDC', amount: 89.5, usdValue: 89.5 },
+];
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <WalletDashboard {...dummyWallet} />
+    <>
+      <WalletDashboard {...dummyWallet} />
+      <WalletTokenTable tokens={mockTokens} />
+    </>
   </React.StrictMode>
 );
-
